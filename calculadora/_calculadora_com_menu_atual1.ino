5d8f7e7c2a979c51119df48b21dade84042c17b9
Silvia <im.tchubi@gmail.com>
2025-05-16 14:15:45 -0300

    add calculadora

--- calculadora/_calculadora_com_menu_atual1.ino
+++ calculadora/_calculadora_com_menu_atual1.ino
@@ -1,14 +1,36 @@
 int choiceOfOperation;
-float number1;
-float number2;
+float result;
+
+void sum(float num1, float num2){
+  result = num1 + num2;
+  Serial.println("nice!! :D the sum result is: \n" + String(result));
+}
+  
+void subtract(float num1, float num2){
+  result = num1 - num2;
+  Serial.println("nice!! :D the subtraction result is: \n" + String(result));
+}
+   
+void divide(float num1, float num2){
+  result = num1 / num2;
+  Serial.println("nice!! :D the division result is: \n" + String(result));
+}
+  
+void multiply(float num1, float num2){
+  result = num1 * num2;
+  Serial.println("nice!! :D the multiplication result is: \n" + String(result));
+}
 
 void setup()
 {
   Serial.begin(9600);
   
   int counter = 0; //initialized variable of 'do while'
+  float number1;
+  float number2;
   
   do{
+    
     Serial.println("Welcome to the smart calculator !! :p\n");
     Serial.println("Choose one of the operations:");
     Serial.println("1- sum");
@@ -31,23 +53,31 @@ void setup()
     
     switch(choiceOfOperation){
       case 1:
+      sum(number1, number2);
+      //sum(number1);
+      //float returnSumFunction;
+      //returnSumFunction = sum(number1, number2);
+      //Serial.println(returnSumFunction);
       	break;
       
       case 2:
+      subtract(number1, number2);
       	break;
       
       case 3:
+      divide(number1, number2);
       	break;
       
       case 4:
+      multiply(number1, number2);
       	break;
       
       default:
-      	Serial.println("Invalid option:( Please choose an option from 1 to 4!\n");
+      	Serial.println("Invalid option :( Please choose an option from 1 to 4!\n");
     }
     
     
-    Serial.println("Do you want to return to the menu? Type yes or no (in lowercase, please)");
+    Serial.println("Would you like to return to the menu? Type yes or no (in lowercase, please)");
     while(!Serial.available()){}
     
     
